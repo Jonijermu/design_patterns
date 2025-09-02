a1c6206b6593a7555d06f3680c8332ff58e8486f
jonijermu <joni.heikkila@metropolia.fi>
2025-09-02 08:56:31 +0300

    Refactor Logger class to improve exception handling and enforce singleton pattern

--- singleton/src/main/java/Logger.java
+++ singleton/src/main/java/Logger.java
@@ -6,29 +6,45 @@ public class Logger {
     private FileWriter myWriter;
     private static Logger instance;
 
-    public Logger() {
-
+    private Logger() {
+        try {
+            setFileName("default_log.txt");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static Logger getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new Logger();
         }
         return instance;
-
     }
 
-    public void setFileName(String name) throws IOException {
-       myWriter = new FileWriter(name, true);
-
+    public void setFileName(String name) {
+        try {
+            myWriter = new FileWriter(name, true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void write(String text) throws IOException {
-        myWriter.write(text + System.lineSeparator());
-        myWriter.flush();
+    public void write(String text) {
+        try {
+            myWriter.write(text + System.lineSeparator());
+            myWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void close() {
+        try {
+            myWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- singleton/src/main/java/Main.java
+++ singleton/src/main/java/Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Logger logger = Logger.getInstance();
         System.out.println(logger);
-        logger.setFileName("new_log.txt"); // Change file name
+        logger.setFileName("new_log.txt");
         logger.write("Simulation started");
         Logger logger2 = Logger.getInstance();
         logger2.write("prööt");
