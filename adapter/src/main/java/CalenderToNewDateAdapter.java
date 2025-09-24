886c2663dbb26dd7dee89af56a57762377b6383b
joni.heikkila <joni.heikkila@metropolia.fi>
2025-09-24 18:31:44 +0300

    prööt

---
+++ adapter/src/main/java/CalenderToNewDateAdapter.java
@@ -0,0 +1,37 @@
+public class CalenderToNewDateAdapter implements NewDateInterface {
+
+    @Override
+    public void setDay(int day) {
+
+    }
+
+    @Override
+    public void setMonth(int month) {
+
+    }
+
+    @Override
+    public void setYear(int year) {
+
+    }
+
+    @Override
+    public int getDay() {
+        return 0;
+    }
+
+    @Override
+    public int getMonth() {
+        return 0;
+    }
+
+    @Override
+    public int getYear() {
+        return 0;
+    }
+
+    @Override
+    public void advanceDays(int days) {
+
+    }
+}
